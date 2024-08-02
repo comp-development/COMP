115c6391ca0a553a66bbe76038710606a2db0ce4
AV-01 <aryavratmishra007@gmail.com>
2024-08-01 23:37:45 -0700

    Made basic login/signup page
    At least they exist?

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Account from '$lib/components/Account.svelte';
+</script>
+
+<div class="app">
+    <Account logIn = true/>
+
+	<main>
+		<slot />
+	</main>
+
+</div>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Sign Up</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+</section>
