083896c824f4aa69a152ca5caf533deb064e1d71
AV-01 <aryavratmishra007@gmail.com>
2024-08-02 09:10:27 -0700

    Decent-ish login/signup page

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import "carbon-components-svelte/css/white.css";
+	import scheme from "$lib/scheme.json";
+</script>
+
+<div class="header">
+	<img
+		src="/mustang_logo.png"
+		alt={scheme.abbreviation + "_logo"}
+		width="50"
+		height="50"
+	/>
+	<h4 style="">{scheme.title}</h4>
+</div>
+
+<style>
+	.header {
+		padding: 5px;
+		/* color: var(--text-color-light); */
+		display: flex;
+		align-items: center;
+		background-color: #206c24;
+	}
+
+	.header h4 {
+		font-weight: 600;
+		/* font-variant: small-caps; */
+        color:white;
+	}
+</style>
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,11 +1,31 @@
 <script>
 	import Account from '$lib/components/Account.svelte';
+    import Banner from '$lib/components/Banner.svelte';
 </script>
 
 <div class="app">
-    <Account logIn = true/>
-
+    <Banner/>
 	<main>
+        <div class="flex">
+            <div style="background-color: #6fc963; border-radius: 10px; width: fit-content; padding: 20px;position: absolute;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 10px;">
+                <Account logIn=true/>
+                <br>
+            <br>
+
+            <div class="flex">
+                <div class="bottomSection svelte-qnamzc">
+                    <a size="lg" class="link svelte-qnamzc" href="/signup">
+                        Sign Up
+                    </a>
+                </div>
+            </div>
+            </div>
+            
+        </div>
 		<slot />
 	</main>
 
