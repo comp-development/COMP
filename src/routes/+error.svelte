2d2da096cee311834c530a26712de1f0b7806e42
Arpit Ranasaria <arpitr@stanford.edu>
2024-09-27 23:43:26 -0700

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,8 @@
 <h1 class="header" style="margin-bottom: 30px;margin-bottom: 20px;">Error!</h1>
 <p>
 	The buttons on our calculator stopped working
-	<i class="ri-emotion-unhappy-line" />
+	<i class="ri-emotion-unhappy-line" /><br>
+	This is likely because you've tried navigating to a page that doesn't exist!
 </p>
 <br />
 <div class="flex">
