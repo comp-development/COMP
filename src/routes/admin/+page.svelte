2a7590991fcf5514bed4af873a1a59126f13c5e6
Arpit Ranasaria <arpitr@stanford.edu>
2024-10-25 14:43:41 -0700

    change admin routes

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,5 +2,5 @@
     import Button from "$lib/components/Button.svelte";
 </script>
 
-<h1 class="header" style="margin-bottom: 30px;">Welcome to the Admin Portal</h1>
-<Button title="Users" href="/admin/users" />
+<h1 class="header" style="margin-bottom: 30px;">Welcome to the Admin Portal.</h1>
+<p>This page should show the user profile, and have the NavMenu on the top to navigate to any given host/tournament</p>
