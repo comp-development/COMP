298b5e6e223030f877d93d157e138bdcb81d4488
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2024-11-20 14:47:41 -0500

    updates to COMP
    Moved code from COMPOSE to COMP and created new function to display user tables for student events.

---
+++ src/lib/components/UserTable.svelte
@@ -0,0 +1,506 @@
+<script>
+	import {
+		DataTable,
+		DataTableSkeleton,
+		Link,
+		Toolbar,
+		ToolbarContent,
+		ToolbarSearch,
+		Pagination,
+		MultiSelect,
+		Tag
+	} from "carbon-components-svelte";
+	
+	
+	import Problem from "$lib/components/Problem.svelte";
+
+	import Switcher from "carbon-icons-svelte/lib/Switcher.svelte";
+	import { createEventDispatcher } from "svelte";
+	import { Filter } from "carbon-icons-svelte";
+	import toast from "svelte-french-toast";
+	
+	import { LogarithmicScale } from "chart.js";
+
+	export let students = [];
+	export let selectable = false;
+	export let stickyHeader = false;
+	export let selectedItems = [];
+	export let nonselectableItems = [];
+	export let sortKey = "created_at";
+	export let sortDirection = "descending";
+	export let editable = true;
+	export let disableAll = false; // disables everything from being selectable
+	export let customHeaders = [];
+	export let draggable = false;
+	export let pageEnabled = true;
+	export let minWidth = 100;
+
+	export let showList = [
+		"full_name",
+		"topics_short",
+		"average_difficulty",
+		"average_quality",
+		"status",
+		"feedback_status",
+		"problem_tests",
+		"created_at"
+	];
+
+	const dispatch = createEventDispatcher();
+
+	let width = 0;
+	$: maxCols = Math.floor((width - 100) / minWidth);
+	$: colWidth = (width - 100) / Math.min(maxCols, showList.length);
+
+	let mobileFriendly = {
+		Algebra: "Alg",
+		Mixed: "Mx",
+		"Number Theory": "NT",
+		Combination: "Comb",
+		Geometry: "Geo",
+	};
+
+	let pageSize = 25;
+	let page = 1;
+
+	let editHeader = { key: "edit", value: "", width: "50px" };
+
+	let headers = [
+		{
+			key: "full_name",
+			value: "Author",
+			short: "Author",
+			icon: "ri-user-fill",
+			width: "10%"
+		},
+		{
+			key: "topics_short",
+			value: "Topics",
+			short: "Topics",
+			icon: "ri-pie-chart-2-fill",
+			width: "15%"
+		},
+		{
+			key: "sub_topics",
+			value: "Subtopics",
+			short: "SubTps",
+			icon: "ri-node-tree",
+			width: "20%"
+		},
+		{
+			key: "average_difficulty",
+			value: "Difficulty",
+			short: "Diff",
+			icon: "ri-bar-chart-2-fill",
+			width: "7%"
+		},
+		{
+			key: "average_quality",
+			value: "Quality",
+			short: "Qlty",
+			icon: "ri-star-fill",
+			width: "7%"
+		},
+		{
+			key: "unresolved_count",
+			value: "Feedback",
+			short: "Fdbk",
+			icon: "ri-flag-fill",
+			width: "10%"
+		},
+		{
+			key: "status",
+			value: "Stage",
+			short: "Stage",
+			icon: "ri-stairs-fill",
+			width: "10%",
+			sort: (a, b) => {
+				const order = ['Draft', 'Idea', 'Endorsed', 'On Test', 'Published', 'Archived'];
+				return order.indexOf(a) - order.indexOf(b);
+			}
+		},
+		{
+			key: "feedback_status",
+			value: "Status",
+			short: "Status",
+			icon: "ri-feedback-fill",
+			width: "10%",
+			sort: (a, b) => {
+				const order = ['Needs Review', 'Awaiting Feedback', 'Awaiting Endorsement', 'Awaiting Testsolve', 'Complete'];
+				return order.indexOf(a) - order.indexOf(b);
+			}
+		},
+		{
+			key: "problem_tests",
+			value: "Tests",
+			short: "Tests",
+			icon: "ri-file-list-3-fill",
+			width: "15%"
+		},
+		{
+			key: "created_at",
+			value: "Created",
+			short: "Create",
+			icon: "ri-calendar-event-fill",
+		},
+		{
+			key: "edited_at",
+			value: "Edited",
+			icon: "ri-calendar-todo-fill",
+		},
+	];
+
+
+	$: headersF = headers.filter((row) => showList.includes(row.key));
+	$: curHeaders = [
+		...(editable ? [editHeader] : []),
+		...[
+			{
+				key: "front_id",
+				value: "ID",
+				icon: "ri-key-2-fill",
+				
+				width: "90px",
+			},
+		],
+		...headersF.slice(0, maxCols),
+		...customHeaders,
+	];
+
+	let tableContainerDiv = null;
+
+	// hacky workaround to not being able to get entire rows
+	function getRowElement(id) {
+		return tableContainerDiv?.querySelector(`[data-row="${id}"]`);
+	}
+
+	let listeners = {};
+	$: if (draggable && tableContainerDiv && !draggingRow) {
+		try {
+			for (let i = 0; i < students.length; i++) {
+				const row = students[i];
+				let elem = getRowElement(row.id);
+				if (row.id in listeners) {
+					elem?.removeEventListener("dragenter", listeners[row.id]);
+				}
+				listeners[row.id] = (e) => handleDragEnter(e, row);
+				elem?.addEventListener("dragenter", listeners[row.id]);
+			}
+		} catch (error) {
+			handleError(error);
+			toast.error(error.message);
+		}
+	}
+
+	let draggingRow = false;
+	let draggedRow = null;
+	let lastDraggedInd = null;
+
+	function startDrag(e, row) {
+		try {
+			if (!draggable) return;
+			draggingRow = true;
+			draggedRow = row;
+		} catch (error) {
+			handleError(error);
+			toast.error(error.message);
+		}
+	}
+
+	function handleDragEnter(e, row) {
+		try {
+			if (!draggable) return;
+			if (!draggingRow) return;
+			if (row === draggedRow) return;
+
+			e.preventDefault();
+			const ind = students.indexOf(row);
+			lastDraggedInd = ind;
+			students.splice(students.indexOf(draggedRow), 1);
+			students.splice(ind, 0, draggedRow);
+			students = students;
+		} catch (error) {
+			handleError(error);
+			toast.error(error.message);
+		}
+	}
+
+	function endDrag(e) {
+		try {
+			if (!draggable) return;
+			if (lastDraggedInd !== null) {
+				dispatch("reorder", {
+					id: draggedRow.id,
+					to: lastDraggedInd,
+				});
+			}
+			draggingRow = false;
+			draggedRow = null;
+			lastDraggedInd = null;
+		} catch (error) {
+			handleError(error);
+			toast.error(error.message);
+		}
+	}
+</script>
+
+<svelte:window />
+<div bind:clientWidth={width} class="align-items: right; display: flex;">
+	<MultiSelect
+		bind:selectedIds={showList}
+		direction="top"
+		size="sm"
+		label="Filter visible columns"
+		items={[
+			{
+				id: "full_name",
+				text: "Author",
+			},
+			{
+				id: "topics_short",
+				text: "Topics",
+			},
+			{
+				id: "sub_topics",
+				text: "SubTopic",
+			},
+			{
+				id: "average_difficulty",
+				text: "Avg. Difficulty",
+			},
+			{
+				id: "average_quality",
+				text: "Avg. Quality",
+			},
+			{
+				id: "status",
+				text: "Stage",
+			},
+			{
+				id: "feedback_status",
+				text: "Status",
+			},
+			{
+				id: "problem_tests",
+				text: "Tests",
+			},
+			{
+				id: "created_at",
+				text: "Created on",
+			},
+			{
+				id: "edited_at",
+				text: "Edited on",
+			},
+			
+		]}
+	/>
+</div>
+
+
+
+<div
+	class="flex-dir-col"
+	on:dragover={(e) => e.preventDefault()}
+	bind:this={tableContainerDiv}
+>
+	<DataTable
+		size="compact"
+		expandable
+		sortable
+		batchExpansion
+		{sortKey}
+		{sortDirection}
+		{selectable}
+		{stickyHeader}
+		bind:selectedRowIds={selectedItems}
+		nonSelectableRowIds={disableAll
+			? students.map((pb) => pb.id)
+			: nonselectableItems}
+		class="datatable"
+		headers={curHeaders}
+		rows={students}
+		pageSize={pageEnabled ? pageSize : undefined}
+		page={pageEnabled ? page : undefined}
+	>
+		<Toolbar size="sm">
+			<ToolbarContent>
+				<ToolbarSearch persistent shouldFilterRows />
+			</ToolbarContent>
+		</Toolbar>
+		<svelte:fragment slot="cell-header" let:header>
+			{#if colWidth > 120}
+				<i class={header.icon} /> {header.value}
+			{:else}
+				<div style="display: flex; align-items: flex-center;">
+					<i
+						class={header.icon}
+						style="display: flex; align-items: flex-center;"
+					/>
+					{header.short ? header.short : header.value}
+				</div>
+			{/if}
+		</svelte:fragment>
+		<svelte:fragment slot="cell" let:row let:header let:cell>
+			<div>
+				{#if cell.key === "edit"}
+					<div class="pencil">
+						<Link class="link" href={"/problems/" + row.id}
+							><i class="ri-pencil-fill" style="font-size: 20px;" /></Link
+						>
+					</div>
+				{:else if cell.key === "drag"}
+					<div
+						draggable={true}
+						on:dragstart={(e) => startDrag(e, row)}
+						on:dragend={(e) => endDrag(e)}
+						style="visibility: {disableAll ? 'hidden' : 'visible'}"
+						class="drag-div"
+					>
+						<div style="margin-left: 10px;"><Switcher /></div>
+					</div>
+				{:else if cell.key === "problem_number"}
+					<div>
+						{cell.value + 1}
+					</div>
+				{:else if cell.key === "topics_short"}
+					<div style="overflow: hidden;">
+						{#if cell.value == null || cell.value == ""}
+							<Tag type="gray">None</Tag>
+						{:else}
+							{#each cell.value.split(", ") as topic}
+								<Tag type="gray">{topic}</Tag>
+							{/each}
+						{/if}
+					</div>
+				{:else if cell.key === "author"}
+					<div style="overflow: hidden;">
+						{cell.value == null || cell.value == ""
+							? "None"
+							: width > 700
+							? cell.value
+							: cell.value.split(" ")[0].charAt(0) +
+							  cell.value.split(" ")[1].charAt(0)}
+					</div>
+				{:else if cell.key === "created_at"}
+					<div style="overflow: hidden;">
+						
+					</div>
+				{:else if cell.key === "edited_at"}
+					<div style="overflow: hidden;">
+						
+					</div>
+				{:else if cell.key === "unresolved_count"}
+					<div style="overflow: hidden;">
+						{cell.value ?? 0}
+					</div>
+				{:else if cell.key === "average_difficulty" || cell.key === "average_quality"}
+					<div
+						style="overflow: hidden; display: flex; align-items: flex-start;"
+					>
+						
+					</div>
+				{:else if cell.key === "status"}
+					<div
+						style="overflow: hidden; display: flex; align-items: flex-start;"
+					>
+						{#if cell.value == "Draft"}
+							<Tag type="gray">Draft</Tag>
+						{:else if cell.value == "Idea"}
+							<Tag type="blue">Idea</Tag>
+						{:else if cell.value == "Endorsed"}
+							<Tag type="cyan">Endorsed</Tag>
+						{:else if cell.value == "On Test"}
+							<Tag type="green">On Test</Tag>
+						{:else if cell.value == "Published"}
+							<Tag type="purple">Published</Tag>
+						{:else if cell.value == "Archived"}
+							<Tag type="high-contrast">Archived</Tag>
+						{/if}
+					</div>
+				{:else if cell.key === "feedback_status"}
+					<div
+						style="overflow: hidden; display: flex; align-items: flex-start;"
+					>
+						{#if cell.value == "Needs Review"}
+							<Tag type="magenta">Needs Review</Tag>
+						{:else if cell.value == "Awaiting Feedback"}
+							<Tag type="blue">Awaiting Feedback</Tag>
+						{:else if cell.value == "Awaiting Endorsement"}
+							<Tag type="teal">Awaiting Endorsement</Tag>
+						{:else if cell.value == "Awaiting Testsolve"}
+							<Tag type="cyan">Awaiting Testsolve</Tag>
+						{:else if cell.value == "Complete"}
+							<Tag type="green">Complete</Tag>
+						{:else if cell.value == "Archived"}
+							<Tag type="high-contrast">Archived</Tag>
+						{/if}
+					</div>
+				{:else if cell.key === "problem_tests"}
+					<div
+						style="overflow: hidden; display: flex; align-items: flex-start;"
+					>
+						{#if row.problem_tests && row.problem_tests.length > 0}
+							{#each row.problem_tests.split(", ") as test}
+								<Tag type="warm-gray">{test}</Tag>
+							{/each}
+						{:else}
+							-
+						{/if}
+					</div>
+				{:else}
+					<div style="overflow: hidden;">
+						{cell.value == null || cell.value == "" ? "-" : cell.value}
+					</div>
+				{/if}
+			</div>
+		</svelte:fragment>
+		<svelte:fragment slot="expanded-row" let:row>
+			<Problem problem={row} />
+		</svelte:fragment>
+	</DataTable>
+	{#if pageEnabled}
+		<Pagination
+			class="datatable"
+			bind:pageSize
+			bind:page
+			totalItems={students.length}
+			pageSizeInputDisabled
+		/>
+	{/if}
+</div>
+
+<style>
+	.drag-div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: grab;
+	}
+
+	.rating {
+		align-items: left;
+	}
+
+	:global(.bx--data-table--sticky-header) {
+		max-height: 800px;
+	}
+
+	:global(.bx--table-header-label) {
+		white-space: nowrap;
+	}
+
+	:global(.bx--data-table-container),
+	:global(.bx--pagination) {
+		width: 100% !important;
+	}
+
+	:global(.bx--table-expand__button) {
+		width: 30px;
+		height: 50px;
+	}
+
+	:global(.bx--list-box__field:focus) {
+		outline-color: var(--primary);
+	}
+</style>
