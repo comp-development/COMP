92a96acdf99fb42bebfd0b4fa0a3dd0c1c65e1c0
Francis Chua <fqchua@stanford.edu>
2025-02-06 10:39:55 -0800

    format student event page

--- src/routes/student/[host_id]/[event_id]/+page.svelte
+++ src/routes/student/[host_id]/[event_id]/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { Button, Badge, Tabs, TabItem} from "flowbite-svelte";
+  import { Button, Badge, Tabs, TabItem } from "flowbite-svelte";
   import StudentForm from "$lib/components/StudentForm.svelte";
   import TeamForm from "$lib/components/TeamForm.svelte";
   import Loading from "$lib/components/Loading.svelte";
-  import { Alert } from 'flowbite-svelte';
-  import { InfoCircleSolid } from 'flowbite-svelte-icons';
+  import { Alert } from "flowbite-svelte";
+  import { InfoCircleSolid } from "flowbite-svelte-icons";
   import { user } from "$lib/sessionStore";
   import {
     getEventInformation,
@@ -17,7 +17,7 @@
     type StudentEvent,
     getStudent,
     updateStudentOrgEvent,
-    type Student
+    type Student,
   } from "$lib/supabase";
   import type { Tables } from "../../../../../db/database.types";
   import { supabase, type Get } from "$lib/supabaseClient";
@@ -41,55 +41,75 @@
   let selectedOption = $state("join_org");
 
   const afterTeamSubmit = async () => {
-    // If this callback is called, then the student must've been in the event => student_event is non-null. 
+    // If this callback is called, then the student must've been in the event => student_event is non-null.
     // Also, we assume the TeamForm component created a team => team is not null.
-    await updateStudentTeam(student_event!.student_event_id, team!.team_id, team!.org_id);
+    await updateStudentTeam(
+      student_event!.student_event_id,
+      team!.team_id,
+      team!.org_id,
+    );
     student_event!.team = team!;
-  }
+  };
 
   const teamJoinSubmit = async (_: Event) => {
     try {
-      team = await getTeamByJoinCode(event_id, teamJoinFormResponses.team_join_code.toUpperCase());
+      team = await getTeamByJoinCode(
+        event_id,
+        teamJoinFormResponses.team_join_code.toUpperCase(),
+      );
     } catch (e) {
       const error = e as any;
       if (error.code === "PGRST116") {
         teamJoinFormErrors["team_join_code"] = "No team with code";
-        return
+        return;
       } else {
-        error.message = `Error getting team: ${error.message}`
+        error.message = `Error getting team: ${error.message}`;
         handleError(error);
       }
     }
-    console.log("team", team)
+    console.log("team", team);
     if (team) {
-      await updateStudentTeam(student_event!.student_event_id, team.team_id, team.org_id);
+      await updateStudentTeam(
+        student_event!.student_event_id,
+        team.team_id,
+        team.org_id,
+      );
       student_event!.team = team;
     } else {
-      throw new Error("An unknown error has occurred. Please email the tournament organizers.");
+      throw new Error(
+        "An unknown error has occurred. Please email the tournament organizers.",
+      );
     }
-  }
+  };
 
   const orgJoinSubmit = async (_: Event) => {
     try {
-      org_event = await getOrgEventByJoinCode(event_id, orgJoinFormResponses.org_join_code.toUpperCase());
+      org_event = await getOrgEventByJoinCode(
+        event_id,
+        orgJoinFormResponses.org_join_code.toUpperCase(),
+      );
     } catch (e) {
       const error: any = e;
       if (error.code === "PGRST116") {
         orgJoinFormErrors["org_join_code"] = "No organization with code";
-        return
+        return;
       } else {
-        error.message = `Error getting org: ${error.message}`
+        error.message = `Error getting org: ${error.message}`;
         handleError(error);
       }
     }
     if (org_event) {
-      await updateStudentOrgEvent(student_event!.student_event_id, org_event.org_id);
+      await updateStudentOrgEvent(
+        student_event!.student_event_id,
+        org_event.org_id,
+      );
       student_event!.org_event = org_event;
     } else {
-      throw new Error("An unknown error has occurred. Please email the tournament organizers.");
+      throw new Error(
+        "An unknown error has occurred. Please email the tournament organizers.",
+      );
     }
-  }
-
+  };
 
   (async () => {
     // Check if this student is registered in this event.
@@ -102,14 +122,21 @@
     team = student_event?.team;
     org_event = student_event?.org_event;
     team?.student_event.sort((a, b) => {
-      const aValues = [a?.front_id ?? "", a?.student?.first_name ?? "", a?.student?.last_name ?? ""];
-      const bValues = [b?.front_id ?? "", b?.student?.first_name ?? "", b?.student?.last_name ?? ""];
+      const aValues = [
+        a?.front_id ?? "",
+        a?.student?.first_name ?? "",
+        a?.student?.last_name ?? "",
+      ];
+      const bValues = [
+        b?.front_id ?? "",
+        b?.student?.first_name ?? "",
+        b?.student?.last_name ?? "",
+      ];
       return aValues < bValues ? 1 : -1;
     });
 
     console.log("student_event", student_event);
     event_details = await getEventInformation(event_id);
-
 
     loading = false;
   })();
@@ -131,7 +158,6 @@
       <br />
     {/if}
   {/if}
-    
 
   <!-- Additional conditions for team and org information -->
   {#if student_event}
@@ -145,9 +171,9 @@
       <div class="team_info">
         <p style="font-weight: bold; font-size: 20px; align-items: left">
           {#if team?.front_id}
-              <Badge color={"green"}>
-                {team.front_id}
-              </Badge>&nbsp
+            <Badge color={"green"}>
+              {team.front_id}
+            </Badge>&nbsp
           {/if}
           {team?.team_name}
         </p>
@@ -162,7 +188,8 @@
           <!--EESHA PLEASE MAKE THIS PRETTIER I BEG YOU TYY-->
           <div style="display: flex; align-items: center;">
             {#if teamMember.front_id}
-              <Badge color={teamMember.student_id == $user?.id ? "green" : "dark"}
+              <Badge
+                color={teamMember.student_id == $user?.id ? "green" : "dark"}
                 >{teamMember.front_id}</Badge
               >&nbsp
             {/if}
@@ -183,11 +210,15 @@
           <Alert border color="red">
             <InfoCircleSolid slot="icon" class="w-5 h-5" />
             <span class="font-medium">Not assigned to a team!</span>
-            You cannot take tests until you are assigned to a team - reach out to your coach if you believe this is in error.
+            You cannot take tests until you are assigned to a team - reach out to
+            your coach if you believe this is in error.
           </Alert>
         {:else}
           <div class="flex">
-            <Button href={`/student/${$page.params.host_id}/${$page.params.event_id}/tests`} pill>Take Tests</Button>
+            <Button
+              href={`/student/${$page.params.host_id}/${$page.params.event_id}/tests`}
+              pill>Take Tests</Button
+            >
           </div>
         {/if}
       </div>
@@ -200,21 +231,29 @@
             title="Join Organization"
           >
             <h2>Join Organization</h2>
-            <p>Get your organization join code from your organization's coach.</p>
-            <CustomForm fields={[
-              {
-                event_custom_field_id: "org_join_code",
-                key: "org_join_code",
-                label: "Organization Join Code",
-                required: true,
-                regex: /^[A-Za-z0-9]{6}$/,
-                placeholder: "ABC123",
-                value: null,
-                choices: null,
-                editable: true,
-                hidden: false
-              }
-            ]} custom_fields={[]} bind:newResponses={orgJoinFormResponses} bind:validationErrors={orgJoinFormErrors} handleSubmit={orgJoinSubmit}/>
+            <p>
+              Get your organization join code from your organization's coach.
+            </p>
+            <CustomForm
+              fields={[
+                {
+                  event_custom_field_id: "org_join_code",
+                  key: "org_join_code",
+                  label: "Organization Join Code",
+                  required: true,
+                  regex: /^[A-Za-z0-9]{6}$/,
+                  placeholder: "ABC123",
+                  value: null,
+                  choices: null,
+                  editable: true,
+                  hidden: false,
+                },
+              ]}
+              custom_fields={[]}
+              bind:newResponses={orgJoinFormResponses}
+              bind:validationErrors={orgJoinFormErrors}
+              handleSubmit={orgJoinSubmit}
+            />
           </TabItem>
           <TabItem
             on:click={() => (selectedOption = "join_team")}
@@ -222,28 +261,44 @@
             title="Join Team"
           >
             <h2>Join Team</h2>
-            <p>Get the code from your coach if you're a part of an org, or an already registered team member for independent teams.</p>
-            <CustomForm fields={[
-              {
-                event_custom_field_id: "team_join_code",
-                key: "team_join_code",
-                label: "Team Join Code",
-                required: true,
-                regex: /^[A-Za-z0-9]{6}$/,
-                placeholder: "ABC123",
-                value: null,
-                choices: null,
-                editable: true,
-                hidden: false
-              }
-            ]} custom_fields={[]} bind:newResponses={teamJoinFormResponses} bind:validationErrors={teamJoinFormErrors} handleSubmit={teamJoinSubmit}/>
+            <p>
+              Get the code from your coach if you're a part of an org, or an
+              already registered team member for independent teams.
+            </p>
+            <CustomForm
+              fields={[
+                {
+                  event_custom_field_id: "team_join_code",
+                  key: "team_join_code",
+                  label: "Team Join Code",
+                  required: true,
+                  regex: /^[A-Za-z0-9]{6}$/,
+                  placeholder: "ABC123",
+                  value: null,
+                  choices: null,
+                  editable: true,
+                  hidden: false,
+                },
+              ]}
+              custom_fields={[]}
+              bind:newResponses={teamJoinFormResponses}
+              bind:validationErrors={teamJoinFormErrors}
+              handleSubmit={teamJoinSubmit}
+            />
           </TabItem>
           <TabItem
             on:click={() => (selectedOption = "create_team")}
             open={selectedOption === "create_team"}
             title="Create Independent Team"
           >
-            <TeamForm bind:team={team} title="Create Independent Team" userType="student" user={student} event_id={event_id} afterSubmit={afterTeamSubmit} />
+            <TeamForm
+              bind:team
+              title="Create Independent Team"
+              userType="student"
+              user={student}
+              {event_id}
+              afterSubmit={afterTeamSubmit}
+            />
           </TabItem>
         </Tabs>
         <br />
@@ -260,7 +315,12 @@
     {/if}
     -->
   {/if}
-  <StudentForm title={student_event ? "Update Information" : "Register"} bind:student_event={student_event} user={{...student, ...$user}} event_id={event_id} />
+  <StudentForm
+    title={student_event ? "Update Information" : "Register"}
+    bind:student_event
+    user={{ ...student, ...$user }}
+    {event_id}
+  />
 {/if}
 
 <style>
@@ -274,8 +334,6 @@
     border: 2px solid var(--primary-light);
     border-radius: 10px;
   }
-
-  
 
   form {
     border: 3px solid var(--primary-tint);
