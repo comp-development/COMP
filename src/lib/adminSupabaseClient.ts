a517f3a6db61f286ea58d5809efee1b16520a84d
Arpit Ranasaria <arpitr@stanford.edu>
2025-02-08 08:25:44 -0500

    Update adminSupabaseClient.ts

--- src/lib/adminSupabaseClient.ts
+++ src/lib/adminSupabaseClient.ts
@@ -3,7 +3,7 @@ import type { Database } from "../../db/database.types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-const supabaseServiceKey = import.meta.env.SUPABASE_SERVICE_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
 // Only for use in api routes.
 export const adminSupabase = createClient<Database>(
