c9f8137d0aa419fd52a11e2a76bf47baf74cba8b
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2025-03-02 20:49:52 -0800

    coach mobile

--- src/routes/coach/[org_id]/[host_id]/[event_id]/+page.svelte
+++ src/routes/coach/[org_id]/[host_id]/[event_id]/+page.svelte
@@ -638,6 +638,12 @@
     margin: 20px;
   }
 
+  @media only screen and (max-width: 900px) {
+    .grid-container {
+      grid-template-columns: 100%;
+    }
+  }
+
   .teams-grid {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
