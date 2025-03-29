65f2a7a4683c1a19a3632957a365b27171abdad6
Francis Chua <francischua689@gmail.com>
2025-03-29 14:15:14 -0700

    reorder migrations for prod

--- db/supabase/migrations/20250329160516_addon_restructure_2.sql
+++ db/supabase/migrations/20250329160516_addon_restructure_2.sql
@@ -1,2 +1,2 @@
 -- Add ordering column to addons table
-ALTER TABLE \"public\".\"addons\" ADD COLUMN \"ordering\" INTEGER NOT NULL DEFAULT 0
+ALTER TABLE "public"."addons" ADD COLUMN "ordering" INTEGER NOT NULL DEFAULT 0
